1feef13509b92dd78a9c76590c277ba066a7b4a0
AnnaRDube <dubearose@gmail.com>
2021-10-22 13:02:06 -0500

    Assignment 7 commit 2

--- apps/assignment07/assignment07.cpp
+++ apps/assignment07/assignment07.cpp
@@ -17,12 +17,12 @@ using namespace std;
 int z = 0;
 
 struct BTree {
-	size_t treeSize = 0;
+	size_t treeSize = 1;
 	vector<int> vptr;
 	Tree<int> binaryTree;
 };
 
-int partition(BTree *ptr, int start, int end) {
+int partition(BTree* ptr, int start, int end) {
 	int pivot = ptr->vptr[end];
 	int pIndex = start;
 	for (int i = start; i < end; i++) {
@@ -35,7 +35,7 @@ int partition(BTree *ptr, int start, int end) {
 	return pIndex;
 }
 
-void quicksort(BTree *ptr, int n) {
+void quicksort(BTree* ptr, int n) {
 	stack<pair<int, int>> s;
 	int start = 0;
 	int end = n - 1;
@@ -54,35 +54,7 @@ void quicksort(BTree *ptr, int n) {
 }
 
 template <typename T>
-void searchTree(BTree* listOfElem, T val, bool& gotIt,
-	int& last, int& first, int& mid, int& pos) {
-/*	if (listOfElem->binaryTree[mid] == val)
-	{
-		gotIt = true;
-		pos = mid;
-	}
-	else if (listOfElem->binaryTree[mid] > val)
-		last = mid - 1;
-	else
-		first = mid + 1;*/
-}
-
-template <typename T>
-void searchVec(BTree* listOfElem, T val, bool &gotIt, 
-	int &last, int &first, int &mid, int &pos) {
-	if (listOfElem->vptr[mid] == val)
-	{
-		gotIt = true;
-		pos = mid;
-	}
-	else if (listOfElem->vptr[mid] > val)
-		last = mid - 1;
-	else
-		first = mid + 1;
-}
-
-template <typename T>
-int binarySearch(BTree *listOfElem, int num, T val, bool isVec) {
+int binarySearch(BTree* listOfElem, int num, T val) {
 	int first = 0,
 		last = num - 1,
 		mid,
@@ -91,76 +63,76 @@ int binarySearch(BTree *listOfElem, int num, T val, bool isVec) {
 	while (!gotIt && first <= last)
 	{
 		mid = (first + last) / 2;
-		if (isVec) {
-			searchVec(listOfElem, val, gotIt, last, first, mid, pos);
+		if (listOfElem->vptr[mid] == val)
+		{
+			gotIt = true;
+			pos = mid;
 		}
-		else {
-			searchTree(listOfElem, val, gotIt, last, first, mid, pos);
-		}
+		else if (listOfElem->vptr[mid] > val)
+			last = mid - 1;
+		else
+			first = mid + 1;
 	}
 	return pos;
 }
 
 BTree* ptrToTree(int s) {
-	unsigned int seedX = time(0);
-	srand(seedX);
 	int num = 0;
 	int searchNum = 0;
 	BTree tree1;
 	tree1.treeSize = s;
 	BTree* ptr = &tree1;
 	int pos = 0;
+	bool inTree = false;
+	chrono::duration<double> elasped_seconds_vec, 
+		elasped_seconds_tree;
 
+
+	srand(time(0));
+	//initialize vector
 	for (int x = 0; x < s; x++) {
-		num = (rand() % (100 - 1 + 1)) + 1;
+		num = (rand() % (10000 - 1 + 1)) + 1;
 		tree1.vptr.push_back(num);
 	}
 	quicksort(ptr, s);
-	for (int x = 0; x < s; x++) {
-		cout << tree1.vptr[x] << " ";
-	}
-	cout << endl;
 
-	//test vector
-	auto start = chrono::steady_clock::now();
+	// initialize tree
+	for (int r = 0; r < s; r++) {
+		tree1.binaryTree = tree1.binaryTree.insert(tree1.vptr[r]);
+	}
+
+	//test vector and tree
 	for (int x = 0; x < 100; x++) {
-		searchNum = (rand() % (100 - 1 + 1)) + 1;
-		pos = binarySearch(ptr, s, searchNum, true);
-	}
-	auto end = chrono::steady_clock::now();
-	chrono::duration<double> elasped_seconds_vec = end - start;
+		searchNum = (rand() % (10000 - 1 + 1)) + 1;
 
-	auto f2 = [](int thing)->void {std::cout << thing; };
-	for (int x = 0; x < s; x++) {
-		tree1.binaryTree.insert(tree1.vptr[x]);
-	}
-	tree1.binaryTree.inorder(f2);
-	cout << endl;
+		auto start = chrono::steady_clock::now();
+		pos = binarySearch(ptr, s, searchNum);
+		auto end = chrono::steady_clock::now();
+		if (x == 0) { elasped_seconds_vec = end - start; }
+		else { elasped_seconds_vec += (end - start); }
 
-	//test binary tree
-	start = chrono::steady_clock::now();
-	for (int x = 0; x < 100; x++) {
-		searchNum = (rand() % (100 - 1 + 1)) + 1;
-		//pos = binarySearch(ptr, s, searchNum, false);
+		start = chrono::steady_clock::now();
+		inTree = tree1.binaryTree.member(searchNum);
+		end = chrono::steady_clock::now();
+		if (x == 0) { elasped_seconds_tree = end - start; }
+		else { elasped_seconds_tree += (end - start); }
 	}
-	end = chrono::steady_clock::now();
-	chrono::duration<double> elasped_seconds_tree = end - start;
 	
 	if (z == 0) {
-		cout << setw(6) << "Size" << setw(15) << "Vector Time"
-			<< setw(15) << "Tree Time" << endl;
+		cout << setw(6) << "Size" << setw(20) << "Vector Time"
+			<< setw(20) << "Tree Time" << endl;
 		cout << "--------------------------------------------------\n";
 	}
-	cout << setw(5) << s << setw(15) << elasped_seconds_vec.count() 
-		<< setw(15) << elasped_seconds_tree.count() << endl;
+	cout << setw(5) << s << setw(20) << (elasped_seconds_vec.count() / 100) << "s"
+		<< setw(20) << (elasped_seconds_tree.count() / 100) << "s" << endl;
 
 	return ptr;
 }
 
 int main() {
 	BTree* Tptr;
-	int size = 10;
-	for (int x = 0; x < 1; x++) {
+	int size = 1000;
+	for (int x = 0; x < 3; x++) {
 		Tptr = ptrToTree(size);
 		size *= 10;
 		z++;
